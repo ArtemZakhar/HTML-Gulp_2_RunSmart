22fc1c8b234c069063fe25fb2266fa68e3684443
Artem Zakharchuk <artemzakharchuk@gmail.com>
2022-06-13 17:50:40 +0300

    before optimization2

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,48 @@ $(document).ready(function() {
   validateForms ('#consultation form');
 
   $('input[name=phone]').mask("+38 (999) 999-9999");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if(!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a").on('click', function() {
+    if (this.hash !== "") {
+      const hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  new WOW().init();
 });
